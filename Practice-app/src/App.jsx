09d987f94d410c6e9cccd664f4baa719a66365af
Suraj Sharma <112156492+Iamsuraj00@users.noreply.github.com>
2026-01-09 10:41:43 +0530

    First react app

---
+++ Practice-app/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+       
+      <h1>Hello World!</h1>
+      <p>My first React application.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
+
+//npm run dev
+//http://localhost:5173/
